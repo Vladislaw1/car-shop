bf5ff50be7ba1a06049cdd43d9467b4047e79d10
Vladislaw1 <vladzajcenko077@gmail.com>
2023-04-20 23:52:43 +0300

    Добавил Actions

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import './App.css';
-import { RouterProvider } from 'react-router';
-import { routing } from './routing';
+import React from "react";
+import "./App.css";
+import { RouterProvider } from "react-router";
+import { routing } from "./routing";
 
 const App = () => <RouterProvider router={routing} />;
-
 export default App;
